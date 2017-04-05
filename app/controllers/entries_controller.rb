25375cdf344dffca99da9ef78a75cc0bbbe67914
netznarkose <mmaintz@posteo.de>
2017-04-05 18:03:51 +0200

    refactor entries_controller #index

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,16 +2,16 @@ class EntriesController < ApplicationController
   require 'leo_mp3_parser'
   before_action :authorize
   before_action :set_entry, only: [:destroy]
+  helper_method :languages, :last_selected_language
 
   def index
     @entries = Entry.for_user(current_user)
-    # @entry = Entry.new
-    # @last_entry_language = Entry.all.last.language rescue 'ende'
-    # @languages_manual = { 'ende' => 'English', 'esde' => 'Spanish', 'frde' => 'French' }
   end
+
 
   def create
     @entry = Entry.new(entry_params)
+    @entry.user_id = current_user.id
     @entry.url = generate_url
     if @entry.save
       redirect_to entries_path, notice: 'Entry was successfully created.'
@@ -32,17 +32,29 @@ class EntriesController < ApplicationController
 
   private
 
-  def generate_url
-    leo_mp3_parser = LeoMp3Parser.new
-    language_and_term = { language: @entry.language, term: @entry.term }
-    leo_mp3_parser.get_audio_url(language_and_term)
-  end
-
   def set_entry
     @entry = Entry.find(params[:id])
   end
 
   def entry_params
     params.require(:entry).permit(:term, :language, :url)
+  end
+
+  def generate_url
+    leo_mp3_parser = LeoMp3Parser.new
+    language_and_term = { language: @entry.language, term: @entry.term }
+    leo_mp3_parser.get_audio_url(language_and_term)
+  end
+
+  def languages
+    {
+      'ende' => 'English',
+      'esde' => 'Spanish',
+      'frde' => 'French'
+    }
+  end
+
+  def last_selected_language
+    @last_selected_language ||= Entry.all.last.language rescue 'ende'
   end
 end
