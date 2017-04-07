f71043f6bc07ac89938944fe5ef2313feaae0a58
netznarkose <mmaintz@posteo.de>
2017-04-07 10:15:38 +0200

    source out entry-metods to entry-helper

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,5 @@
 class EntriesController < ApplicationController
+  include EntriesHelper
   require 'leo_mp3_parser'
   before_action :authorize
   before_action :set_entry, only: [:destroy]
@@ -47,33 +48,5 @@ class EntriesController < ApplicationController
     leo_mp3_parser = LeoMp3Parser.new
     language_and_term = { language: @entry.language, term: @entry.term }
     leo_mp3_parser.get_audio_url(language_and_term)
-  end
-
-  def languages
-    {
-      'ende' => 'English',
-      'esde' => 'Spanish',
-      'frde' => 'French'
-    }
-  end
-
-  def store_in_session(last_selected_language)
-    session[:last_key] = last_selected_language
-  end
-
-  def last_selected_language
-    @last_selected_language ||= session[:last_key]
-  end
-
-  def last_selected_language_simple_form_conversion
-    simple_form_conversion(languages, last_selected_language)
-  end
-
-  def languages_simple_form_conversion
-    languages.map { |key, _value| simple_form_conversion(languages, key) }
-  end
-
-  def simple_form_conversion(hash, key)
-    [hash[key], key]
   end
 end
--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,29 @@
 module EntriesHelper
+  def languages
+    {
+      'ende' => 'English',
+      'esde' => 'Spanish',
+      'frde' => 'French'
+    }
+  end
+
+  def store_in_session(last_selected_language)
+    session[:last_key] = last_selected_language
+  end
+
+  def last_selected_language
+    @last_selected_language ||= session[:last_key]
+  end
+
+  def last_selected_language_simple_form_conversion
+    simple_form_conversion(languages, last_selected_language)
+  end
+
+  def languages_simple_form_conversion
+    languages.map { |key, _value| simple_form_conversion(languages, key) }
+  end
+
+  def simple_form_conversion(hash, key)
+    [hash[key], key]
+  end
 end
